536ad23afe917f008ac6cfdec7e6b64c73a6376c
Jay Dally-Sparre <jaydallysparre@gmail.com>
2024-09-22 20:43:24 +0930

    removed duplicates

--- Commando.cpp
+++
@@ -1,9 +0,0 @@
-#include "Commando.h"
-
-Commando::Commando(InputHandler* inputHandler, sf::Vector2f spawnPos, Team team) : RangedEntity(inputHandler, spawnPos, team) {
-    spriteTexture.loadFromFile("commando_idle.png");
-    sprite.setTexture(spriteTexture);
-    sprite.setHitbox({8.f, 0.f, 16.f, 32.f});
-    speed = 150;
-    health = 300;
-}
--- HBSprite.cpp
+++
@@ -1,9 +0,0 @@
-#include "HBSprite.h"
-
-void HBSprite::setHitbox(const sf::FloatRect& hitbox) {
-    this->hitbox = hitbox;
-}
-
-sf::FloatRect HBSprite::getGlobalHitbox() const {
-    return getTransform().transformRect(hitbox);
-}
